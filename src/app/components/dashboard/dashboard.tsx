ce49b81c820a96f610ff21d1789fe5cd47c2ba03
JC <jcsmileyjr@gmail.com>
2024-06-26 11:44:27 -0500

    feat (landing page): Landing page links leads to their support pages
    Setup a bare bones Journal & Logs page that is linked to the landing page.

--- src/app/components/dashboard/dashboard.tsx
+++ src/app/components/dashboard/dashboard.tsx
@@ -9,7 +9,7 @@ import DashboardPrompt from '../dashboardPrompt/DashboardPrompt';
 const Dashboard = () => {
     return (
         <section className='flex flex-col'>            
-                <Link className="flex justify-center items-center mt-8" href='/content'>
+                <Link className="flex justify-center items-center mt-8" href='/logs'>
                     <button type="button" className="bg-primaryGreen hover:bg-primaryGreen/90 text-white font-bold w-2/3 md:w-1/3 m-auto py-2 px-4 rounded-full">View Logs</button>
                 </Link>
                 <h3 className='text-center text-primaryGreen font-semibold text-2xl mt-12'>Click a writing prompt below</h3>    
--- src/app/components/dashboardPrompt/DashboardPrompt.tsx
+++ src/app/components/dashboardPrompt/DashboardPrompt.tsx
@@ -5,7 +5,7 @@ import Hand from '../../images/left-hand.png';
 const DashboardPrompt = ({content}: {content: string}) => {
     return (
         
-            <Link className='flex justify-center items-center gap-4 text-xl text-primaryBlue mt-6' href='/content'>
+            <Link className='flex justify-center items-center gap-4 text-xl text-primaryBlue mt-6' href='/journal'>
                 {content}
                 <Image src={Hand} className='h-4' alt="" width={20} height={50} style={{width:'auto', height:'auto' }} />
             </Link>
