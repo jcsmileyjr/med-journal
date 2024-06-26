6ef1461259a265f4776eb3a528f7d784bdfab6e5
JC <jcsmileyjr@gmail.com>
2024-06-26 15:32:42 -0500

    Style (landing page): Re-arrange the content

--- src/app/components/dashboard/dashboard.tsx
+++ src/app/components/dashboard/dashboard.tsx
@@ -9,11 +9,6 @@ import DashboardPrompt from '../dashboardPrompt/DashboardPrompt';
 const Dashboard = () => {
     return (
         <section className='flex flex-col'>            
-                <div className=" mt-8">
-                    <Link className=' flex justify-center items-center w-2/3 md:w-1/3 m-auto'  href='/logs'>
-                        <button type="button" className="bg-primaryGreen hover:bg-primaryGreen/90 w-full text-white font-bold  py-2 px-4 rounded-full">View Logs</button>
-                    </Link>                    
-                </div>  
                 <h3 className='text-center text-primaryGreen font-semibold text-2xl mt-12'>Click a writing prompt below</h3>    
                 <DashboardPrompt content="FreeStyle"/>
                 <DashboardPrompt content="Doctor/Hospital Visit"/> 
@@ -21,6 +16,11 @@ const Dashboard = () => {
                 <DashboardPrompt content="What hurts (body sore/injury)"/>
                 <DashboardPrompt content="Unexplained Medical issue"/>
                 <DashboardPrompt content="Prescription medicine"/>     
+                <div className=" mt-12">
+                    <Link className=' flex justify-center items-center w-2/3 md:w-1/3 m-auto'  href='/logs'>
+                        <button type="button" className="bg-primaryGreen hover:bg-primaryGreen/90 w-full text-white font-bold  py-2 px-4 rounded-full">View Logs</button>
+                    </Link>                    
+                </div>  
         </section>
     )
 }
