42206da92fa833d902e3db7a9672cb852cc4ba2d
JC <jcsmileyjr@gmail.com>
2024-06-26 10:08:45 -0500

    Refactor (Header): Create Setting page and Header back link option
    Refactored the Header component's options for a link (based on the mockup design) to return the user to the landing page. Created a bare bones setting page with only a header.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Header from './components/header/header'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col p-8 md:p-24">
-      <Header />
+      <Header displayBack={false} />
       Test Landing Page
     </main>
   )
