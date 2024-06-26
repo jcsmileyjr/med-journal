49a28a5f4ad4e2b6cd33851e1657db9517ff5e4f
JC <jcsmileyjr@gmail.com>
2024-06-26 14:32:14 -0500

    Fix (landing page): Links are to wide
    Bug found where the user can to easily click a link because they are app wide.

--- src/app/components/dashboard/dashboard.tsx
+++ src/app/components/dashboard/dashboard.tsx
@@ -9,9 +9,11 @@ import DashboardPrompt from '../dashboardPrompt/DashboardPrompt';
 const Dashboard = () => {
     return (
         <section className='flex flex-col'>            
-                <Link className="flex justify-center items-center mt-8" href='/logs'>
-                    <button type="button" className="bg-primaryGreen hover:bg-primaryGreen/90 text-white font-bold w-2/3 md:w-1/3 m-auto py-2 px-4 rounded-full">View Logs</button>
-                </Link>
+                <div className=" mt-8">
+                    <Link className=' flex justify-center items-center w-2/3 md:w-1/3 m-auto'  href='/logs'>
+                        <button type="button" className="bg-primaryGreen hover:bg-primaryGreen/90 w-full text-white font-bold  py-2 px-4 rounded-full">View Logs</button>
+                    </Link>                    
+                </div>  
                 <h3 className='text-center text-primaryGreen font-semibold text-2xl mt-12'>Click a writing prompt below</h3>    
                 <DashboardPrompt content="FreeStyle"/>
                 <DashboardPrompt content="Doctor/Hospital Visit"/> 
