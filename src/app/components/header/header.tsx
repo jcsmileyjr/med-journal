5067b805bc8ef5b95e25e575acd9e5e9e4d48d03
JC <jcsmileyjr@gmail.com>
2024-06-26 09:09:51 -0500

    Feat (Header): Add images and content
    Flesh out the component to look like the design with images, styled content, and a link to the setting page.

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -1,12 +1,17 @@
-"use client"
-
-import { useState } from 'react';
+import Image from 'next/image'
+import Link from 'next/link';
+import BookIcon from '../../images/book.png';
+import ProfileIcon from '../../images/profile.jpg';
 
 const Header = () => {
     return (
-        <div>
-            Header
-        </div>
+        <section className='flex flex-row items-center justify-between'>
+            <Image src={BookIcon} className='h-4' alt="" width={20} height={50} style={{width:'auto', height:'auto' }} />
+            <h1 className='text-primaryGreen font-bold text-3xl'>Medical Journal</h1>
+            <Link href='/setting'>
+                <Image src={ProfileIcon} className='h-4' alt="Link to setting page" width={20} height={50} style={{width:'auto', height:'auto' }} />
+            </Link>
+        </section>
     )
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Header from './components/header/header'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col p-24">
+    <main className="flex min-h-screen flex-col p-8 md:p-24">
       <Header />
       Test Landing Page
     </main>
