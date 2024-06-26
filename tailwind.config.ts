b1ba587812fd78f4e5571ad904ce5b2afe3fb5b7
JC <jcsmileyjr@gmail.com>
2024-06-26 10:32:06 -0500

    Style (landing page): Setup a responsive design page title

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,10 +7,10 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    colors: {
-      'primaryGreen': '#00613F',
-    },    
     extend: {
+      colors: {
+        'primaryGreen': '#00613F',
+      },    
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
