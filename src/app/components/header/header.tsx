6a613601311d0e397689d8798e6013e781764ec3
JC <jcsmileyjr@gmail.com>
2024-06-26 08:40:09 -0500

    Feat (landing page): Header component
    Create initial bare bones Header component for the UI.

---
+++ src/app/components/header/header.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import { useState } from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            Header
+        </div>
+    )
+}
+
+export default Header;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Image from 'next/image'
+import Header from './components/header/header'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col p-24">
+      <Header />
       Test Landing Page
     </main>
   )
