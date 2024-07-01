96443cdae4bfa0937b98f312c9d404fd56066615
JC <jcsmileyjr@gmail.com>
2024-06-30 22:28:21 -0500

    Feature (Logs): Initial function to grab the data

--- src/app/logs/page.tsx
+++ src/app/logs/page.tsx
@@ -1,10 +1,28 @@
+"use client"
+import {useState, useEffect} from 'react';
+import getData from '../utils/getData';
 import Header from '../components/header/header';
 
 const Logs = () => {
+    const [content, setContent] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const data = await getData();
+            setContent(data);
+        }
+        fetchData();
+    }, [])
+
     return (
         <main className="flex min-h-screen flex-col p-8 md:p-24 md:pt-8">
             <Header displayBack={true} />
-            Content Page
+
+            {content.length < 1 && <h1>Loading...</h1>}
+            {content.length > 0 &&
+                <p>Content</p>
+            }
+            
         </main>
     )
 }
---
+++ src/app/utils/getData.ts
@@ -0,0 +1,15 @@
+import  secureLocalStorage  from  "react-secure-storage";
+import ContentType from '../types/contentType';
+
+const getData = () => {
+    let previousData = secureLocalStorage.getItem("medical-journal");
+    if (typeof(previousData) === "string") {
+        let contentArray = JSON.parse(previousData);
+        console.log("contentArray", contentArray);
+        return contentArray;
+    } else {
+        return [];
+    }
+}
+
+export default getData;
