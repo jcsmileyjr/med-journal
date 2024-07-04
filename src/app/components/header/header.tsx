497c6b71f81438436c2da99d0a4a4c10ce15db7f
JC <jcsmileyjr@gmail.com>
2024-07-04 13:22:26 -0500

    Style (Header): Accessibilty updates
    1. Color update on Logs page
    2. Add aria-labels to links

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -9,13 +9,13 @@ import ProfileIcon from '../../images/profile.jpg';
 const Header = ({displayBack}: {displayBack: boolean}) => {
     return (
         <header className='flex flex-row items-center justify-between'>
-            <Link href='/'>
+            <Link aria-label="Home page" href='/'>
                 <Image src={BookIcon} className='h-4' alt="" width={20} height={50} style={{width:'auto', height:'auto' }} />
             </Link>
             <h1 className='text-primaryGreen font-bold text-3xl'>Medical Journal</h1>
-            {displayBack && <Link href='/'><p className='text-red-400 text-base'>Back</p></Link>}
+            {displayBack && <Link aria-label="Home page" href='/'><p className='text-red-800 text-base'>Back</p></Link>}
             {!displayBack && 
-                <Link href='/setting'>
+                <Link aria-label="Settingpage" href='/setting'>
                     <Image src={ProfileIcon} className='h-4' alt="Link to setting page" width={20} height={50} style={{width:'auto', height:'auto' }} />
                 </Link>
             }
