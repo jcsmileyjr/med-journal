87aa680fee9b49b113113b719f02af194bebfe17
JC <jcsmileyjr@gmail.com>
2024-07-05 06:38:34 -0500

    Test (author): Initial component test

---
+++ src/app/components/author/__tests__/author.test.tsx
@@ -0,0 +1,18 @@
+import {render, screen, fireEvent} from '@testing-library/react';
+import Author from '../author';
+
+// Mock useRouter:
+jest.mock("next/navigation", () => ({
+useRouter() {
+    return {
+        prefetch: () => null
+        };
+    } 
+}));
+
+describe('Author', () => {
+    it('renders correctly', () => {
+        render(<Author content="Wellness Check-in" />);
+        expect(screen.getByText("Let's talk about your wellness check-in")).toBeInTheDocument();
+    });
+});
