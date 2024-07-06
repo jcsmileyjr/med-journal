47a7d3222cca70f7121196f9528dc394b5c73d66
JC <jcsmileyjr@gmail.com>
2024-07-06 07:39:18 -0500

    Refactor (Edit Entry): Remove references to tag as its no longer needed

--- src/app/editEntry/[id]/page.tsx
+++ src/app/editEntry/[id]/page.tsx
@@ -1,16 +1,14 @@
 "use client"
-import Author from '../../../components/author/author';
-import Header from '../../../components/header/header';
+import Author from '../../components/author/author';
+import Header from '../../components/header/header';
 
 /**
  * Page that allows the user to edit an existing journal entry.
  * @param {any} params
  * @param {string} params.id - The id of the journal entry to be edited. Can be 'none' (meaning not an previous entry)
- * @param {string} params.tag - The tag of the journal entry to be edited
- * @returns 
  */
-const EditEntry = ({ params }: { params: { id: string, tag: string } }) => {
-    const { id, tag } = params;
+const EditEntry = ({ params }: { params: { id: string} }) => {
+    const { id} = params;
     
     return (
         <main className="flex min-h-screen flex-col p-8 md:p-24 md:pt-8">
