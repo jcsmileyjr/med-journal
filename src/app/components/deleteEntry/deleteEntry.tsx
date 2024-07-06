d5347833363c517b3b2880f0717dcd253d36636b
JC <jcsmileyjr@gmail.com>
2024-07-06 11:43:47 -0500

    Feature (DeleteEntry): Logic to delete journal entry from state

--- src/app/components/deleteEntry/deleteEntry.tsx
+++ src/app/components/deleteEntry/deleteEntry.tsx
@@ -1,16 +1,16 @@
 "use client"
-import {useState, useEffect} from 'react';
+import {useState} from 'react';
 import { useRouter } from 'next/navigation';
+import deleteData from '@/app/utils/deleteData';
 import Image from 'next/image';
 import Delete from '../../images/delete.png';
 
 const DeleteEntry = ({id}: {id: string}) => {
     const router = useRouter();
-console.log("DeleteEntry id", id);
     const [toggleUI, setToggleUI] = useState(false);
 
-    const deleteEntry = async () => {
-    
+    const deleteEntry = () => {
+        deleteData(id, router);
     }
 
     return (
@@ -23,7 +23,7 @@ console.log("DeleteEntry id", id);
             }
             {toggleUI &&
                 <div>
-                    <button onClick={() => console.log("works")} className='flex justify-center items-center gap-4 mt-2 w-2/3 md:w-1/3 m-auto border-2 border-rose-600 rounded'>
+                    <button onClick={() => deleteEntry()} className='flex justify-center items-center gap-4 mt-2 w-2/3 md:w-1/3 m-auto border-2 border-rose-600 rounded'>
                         <Image src={Delete} className='h-8 p-2' alt="" width={20} height={50} style={{width:'auto' }} />
                         <p className='text-red-800 text-base font-bold '>Confirm Delete</p>
                     </button>
---
+++ src/app/utils/deleteData.ts
@@ -0,0 +1,21 @@
+import  secureLocalStorage  from  "react-secure-storage";
+import ContentType from '../types/contentType';
+
+/**
+ * Delete a journal entry from local storage. Afterward, redirects the user to the Logs page.
+ * @param {string} id - ID of the entry to be deleted
+ * @param {any} router - The router object.
+ */
+const deleteData = (id: string, router: any) => {    
+    let previousData = secureLocalStorage.getItem("medical-journal");
+console.log("deleteData data");
+    if (typeof previousData === "string") {
+        let contentArray = JSON.parse(previousData);
+        const index = contentArray.findIndex((item: ContentType) => item.id === id);
+        contentArray.splice(index, 1);
+        secureLocalStorage.setItem("medical-journal", JSON.stringify(contentArray));
+    } 
+    router.push("/logs")
+}
+
+export default deleteData;
