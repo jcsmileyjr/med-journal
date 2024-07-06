3e44c4b4502dd1260fcf82b3ee0bdf59e0a46d92
JC <jcsmileyjr@gmail.com>
2024-07-06 07:34:31 -0500

    Docs: Documentation for the Edit & Journal pages

--- src/app/journal/page.tsx
+++ src/app/journal/page.tsx
@@ -3,7 +3,12 @@ import Header from '../components/header/header';
 import Author from '../components/author/author';
 import { useSearchParams } from 'next/navigation'
 
+/**
+ * Page that allows the user to create a journal entry via the Author component.
+ * @returns 
+ */
 const Journal = () => {
+    // Get the tag from the URL's search params
     const searchParams = useSearchParams()
     let search = searchParams.get('tag')
     if (search === null) {
